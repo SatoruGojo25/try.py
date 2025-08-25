6f5d4aae2d4c8f180453cd371fe7ea274c03e067
SatoruGojo25 <edukramil78@gmail.com>
2025-08-24 21:25:42 -0300

    verificando condicao1

---
+++ aula9.py
@@ -0,0 +1,24 @@
+#if / elif / else 
+#se / se não se / se não 
+
+# entrada = input('Você quer "entrar" ou "sair"? ')
+# if entrada == 'entrar':
+#     print ('voce escolheu a morte ne \n voce ta querendo ir aonde meu?')
+# elif entrada == 'sair':
+#     print("escolheu vida ne ")
+# else :
+#     print("voce é burro olha a pergunta! ")
+condicao = False 
+condicao1 = False 
+condicao2 = False 
+condicao3 = True
+
+
+if condicao:
+    print('este é o codigo do if ')
+else:
+    print ('este é o else do primeiro if')  
+if 10 == 10:
+    print('10 é igual a 10')
+
+print('fora do if')
